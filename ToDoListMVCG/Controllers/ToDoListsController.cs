9a67ea107629a0b6820015c2c905b31f2abee9f7
peshopbs2 <peshopbs2@gmail.com>
2022-04-14 14:57:18 -0800

    add authorize requirement for access to to do list management

--- ToDoListMVCG/Controllers/ToDoListsController.cs
+++ ToDoListMVCG/Controllers/ToDoListsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -10,6 +11,7 @@ using ToDoListMVCG.Data;
 
 namespace ToDoListMVCG.Controllers
 {
+    [Authorize]
     public class ToDoListsController : Controller
     {
         private readonly ApplicationDbContext _context;
